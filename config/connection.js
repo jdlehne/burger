7fc7b78ebfd21d61007249b70aedc46dafa3f09e
Josh Lehne <jdlehne@gmail.com>
2017-09-24 12:03:20 -0500

    bug hunting

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,23 @@
 var mysql = require("mysql");
 
+module.exports = function(app) {
 
-module.exports = function(app){
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "burgers_db"
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "burgers_db"
-});
+  });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("mySql connected at " + connection.threadId);
+  });
 
+    //connection.end();
 
 }
--- server.js
+++ server.js
@@ -2,52 +2,37 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
+
+//require("./controllers/burgers_controller.js")(app);
+require("./config/connection.js")(app);
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 app.use(methodOverride("_method"));
-
-// Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({
   defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
 
+app.use("/", routes);
+
 app.listen(port, function() {
   console.log("I AM ALIVE!!!! @port " + port);
 });
-var routes = require("./controllers/burgers_controller.js");
 
-app.use("/", routes);
-//-----to be exported-----/ in burgers_controller.js
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-require("./controllers/burgers_controller.js")(app);
-require("./config/connection.js")(app);
-
-/*app.get("/", function(req, res) {
+app.get("/", function(req, res) {
   connection.query("SELECT * FROM burgers;", function(err, data) {
     if (err) {
       throw err;
@@ -85,5 +70,3 @@ app.put("/", function(req, res) {
     res.redirect("/");
   });
 });
-
-//app.listen(port); //-----save for export*/
